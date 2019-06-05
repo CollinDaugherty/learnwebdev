65233c52a744ebb43a6da2a1154f9ba80fd20bb7
Collin Daugherty <collindaugherty@protonmail.com>
2019-06-05 11:17:11 -0500

    create nav, footer, and tutorial placeholders

---
+++ src/components/MainContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TutorialForm from './TutorialForm';
+
+class MainContent extends Component {
+  render() {
+    return (
+      <div>
+        <TutorialForm />
+      </div>
+    );
+  }
+}
+
+export default MainContent;
---
+++ src/components/TutorialForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class TutorialForm extends Component {
+  render() {
+    return (
+      <form>
+        <label for='title'>Title</label>
+        <input type='text' name='title' />
+
+        <label for='url'>URL</label>
+        <input type='text' name='url' />
+
+        <label for='categories'>Categories</label>
+        <input type='text' name='categories' />
+
+        <div>
+          <input type='radio' id='free' name='cost' />
+          <label for='free'>Free</label>
+
+          <input type='radio' id='paid' name='cost' />
+          <label for='paid'>Paid</label>
+        </div>
+
+        <div>
+          <input type='radio' id='article' name='medium' />
+          <label for='article'>Article</label>
+
+          <input type='radio' id='video' name='medium' />
+          <label for='video'>Video</label>
+        </div>
+
+        <div>
+          <input type='radio' id='beginner' name='difficulty' />
+          <label for='beginner'>Beginner</label>
+
+          <input type='radio' id='advanced' name='difficulty' />
+          <label for='advanced'>Advanced</label>
+        </div>
+
+        <button>Submit Tutorial</button>
+      </form>
+    );
+  }
+}
+
+export default TutorialForm;
