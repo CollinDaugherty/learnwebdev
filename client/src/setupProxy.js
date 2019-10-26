d3a1b8a1bce98a78ca50a458a0cf33d31b753bc0
Collin Daugherty <collindaugherty@protonmail.com>
2019-10-25 20:46:10 -0500

    edit client proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,9 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://localhost:8080/' }));
+  app.use(
+    proxy('/api', {
+      target: process.env.SERVER_URL || 'http://localhost:8080/'
+    })
+  );
 };
