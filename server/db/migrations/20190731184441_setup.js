286b06911d63bddd931e887409d3d499f50b640b
Collin Daugherty <collindaugherty@protonmail.com>
2019-10-26 10:31:33 -0500

    fix migrations

--- server/db/migrations/20190731184441_setup.js
+++ server/db/migrations/20190731184441_setup.js
@@ -1,120 +1,116 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-        // USERS TABLE
-        knex.schema.createTable('users', t => {
-          t.uuid('id')
-            .primary()
-            .unique()
-            .notNullable();
-          t.string('name').notNullable();
-          t.string('email')
-            .unique()
-            .notNullable();
-          t.string('password').notNullable();
-          t.timestamp('joined', { useTz: false }).notNullable();
-          t.text('website');
-          t.string('github');
-          t.string('twitter');
-          t.text('avatar');
-          t.boolean('is_admin')
-            .defaultTo(false)
-            .notNullable();
-        }),
+    // USERS TABLE
+    knex.schema.createTable('users', t => {
+      t.uuid('id')
+        .primary()
+        .unique()
+        .notNullable();
+      t.string('name').notNullable();
+      t.string('email')
+        .unique()
+        .notNullable();
+      t.string('password').notNullable();
+      t.timestamp('joined', { useTz: false }).notNullable();
+      t.text('website');
+      t.string('github');
+      t.string('twitter');
+      t.text('avatar');
+      t.boolean('is_admin')
+        .defaultTo(false)
+        .notNullable();
+    }),
 
-        // INSTRUCTORS TABLE
-        knex.schema.createTable('instructors', t => {
-          t.uuid('id')
-            .primary()
-            .unique()
-            .notNullable();
-          t.string('name')
-            .unique()
-            .notNullable();
-          t.timestamp('created', { useTz: false }).notNullable();
-          t.text('website');
-          t.string('github');
-          t.string('twitter');
-          t.text('avatar');
-        }),
+    // INSTRUCTORS TABLE
+    knex.schema.createTable('instructors', t => {
+      t.uuid('id')
+        .primary()
+        .unique()
+        .notNullable();
+      t.string('name')
+        .unique()
+        .notNullable();
+      t.timestamp('created', { useTz: false }).notNullable();
+      t.text('website');
+      t.string('github');
+      t.string('twitter');
+      t.text('avatar');
+    }),
 
-        // TUTORIALS TABLE
-        knex.schema.createTable('tutorials', t => {
-          t.string('id')
-            .primary()
-            .unique()
-            .notNullable();
-          t.uuid('user_id').notNullable();
-          t.uuid('instructor_id');
-          t.string('instructor_name');
-          t.string('title').notNullable();
-          t.text('url')
-            .unique()
-            .notNullable();
-          t.timestamp('date', { useTz: false }).notNullable();
-          t.enum('cost', ['free', 'paid']).notNullable();
-          t.enum('medium', ['article', 'video']).notNullable();
-          t.enum('difficulty', ['beginner', 'advanced']).notNullable();
-          t.specificType('categories', 'text ARRAY').notNullable();
-        }),
+    // TUTORIALS TABLE
+    knex.schema.createTable('tutorials', t => {
+      t.string('id')
+        .primary()
+        .unique()
+        .notNullable();
+      t.uuid('user_id').notNullable();
+      t.uuid('instructor_id');
+      t.string('instructor_name');
+      t.string('title').notNullable();
+      t.text('url')
+        .unique()
+        .notNullable();
+      t.timestamp('date', { useTz: false }).notNullable();
+      t.enum('cost', ['free', 'paid']).notNullable();
+      t.enum('medium', ['article', 'video']).notNullable();
+      t.enum('difficulty', ['beginner', 'advanced']).notNullable();
+      t.specificType('categories', 'text ARRAY').notNullable();
+    }),
 
-        // COMMENTS TABLE
-        knex.schema.createTable('comments', t => {
-          t.string('id')
-            .primary()
-            .unique()
-            .notNullable();
-          t.uuid('user_id').notNullable();
-          t.string('tutorial_id').notNullable();
-          t.text('body').notNullable();
-          t.timestamp('date', { useTz: false }).notNullable();
-        }),
+    // COMMENTS TABLE
+    knex.schema.createTable('comments', t => {
+      t.string('id')
+        .primary()
+        .unique()
+        .notNullable();
+      t.uuid('user_id').notNullable();
+      t.string('tutorial_id').notNullable();
+      t.text('body').notNullable();
+      t.timestamp('date', { useTz: false }).notNullable();
+    }),
 
-        knex.schema.createTable('tutorial_votes', t => {
-          t.uuid('id')
-            .primary()
-            .unique()
-            .notNullable();
-          t.string('tutorial_id').notNullable();
-          t.uuid('user_id').notNullable();
-          t.smallint('vote_value');
-        }),
-        knex.schema.createTable('comment_votes', t => {
-          t.uuid('id')
-            .primary()
-            .unique()
-            .notNullable();
-          t.string('comment_id').notNullable();
-          t.uuid('user_id').notNullable();
-          t.smallint('vote_value');
-        }),
-        knex.schema.createTable('favorites', t => {
-          t.uuid('id')
-            .primary()
-            .unique()
-            .notNullable();
-          t.string('tutorial_id').notNullable();
-          t.uuid('user_id').notNullable();
-          t.timestamp('date', { useTz: false }).notNullable();
-        })
-      ]);
+    knex.schema.createTable('tutorial_votes', t => {
+      t.uuid('id')
+        .primary()
+        .unique()
+        .notNullable();
+      t.string('tutorial_id').notNullable();
+      t.uuid('user_id').notNullable();
+      t.smallint('vote_value');
+    }),
+    knex.schema.createTable('comment_votes', t => {
+      t.uuid('id')
+        .primary()
+        .unique()
+        .notNullable();
+      t.string('comment_id').notNullable();
+      t.uuid('user_id').notNullable();
+      t.smallint('vote_value');
+    }),
+    knex.schema.createTable('favorites', t => {
+      t.uuid('id')
+        .primary()
+        .unique()
+        .notNullable();
+      t.string('tutorial_id').notNullable();
+      t.uuid('user_id').notNullable();
+      t.timestamp('date', { useTz: false }).notNullable();
+    })
+  ]);
 
-      console.log('Tables created successfully');
-      resolve();
-    };
+  console.log('Tables created successfully');
+};
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-        knex.schema.dropTable('users'),
-        knex.schema.dropTable('instructors'),
-        knex.schema.dropTable('tutorials'),
-        knex.schema.dropTable('comments'),
-        knex.schema.dropTable('tutorial_votes'),
-        knex.schema.dropTable('comment_votes'),
-        knex.schema.dropTable('favorites')
-      ]);
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('instructors'),
+    knex.schema.dropTable('tutorials'),
+    knex.schema.dropTable('comments'),
+    knex.schema.dropTable('tutorial_votes'),
+    knex.schema.dropTable('comment_votes'),
+    knex.schema.dropTable('favorites')
+  ]);
 
-      console.log('Tables dropped');
-      resolve();
-    } catch (error) {
-      reject(error);
-    };
+  console.log('Tables dropped');
+};
--- server/db/migrations/20190731185917_add_foreign_keys.js
+++ server/db/migrations/20190731185917_add_foreign_keys.js
@@ -1,79 +1,73 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-        knex.schema.alterTable('tutorials', t => {
-          t.foreign('user_id')
-            .references('id')
-            .inTable('users');
-          t.foreign('instructor_id')
-            .references('id')
-            .inTable('instructors');
-        }),
-        knex.schema.alterTable('comments', t => {
-          t.foreign('user_id')
-            .references('id')
-            .inTable('users');
-          t.foreign('tutorial_id')
-            .references('id')
-            .inTable('tutorials');
-        }),
-        knex.schema.alterTable('tutorial_votes', t => {
-          t.foreign('user_id')
-            .references('id')
-            .inTable('users');
-          t.foreign('tutorial_id')
-            .references('id')
-            .inTable('tutorials');
-        }),
-        knex.schema.alterTable('comment_votes', t => {
-          t.foreign('user_id')
-            .references('id')
-            .inTable('users');
-          t.foreign('comment_id')
-            .references('id')
-            .inTable('comments');
-        }),
-        knex.schema.alterTable('favorites', t => {
-          t.foreign('user_id')
-            .references('id')
-            .inTable('users');
-          t.foreign('tutorial_id')
-            .references('id')
-            .inTable('tutorials');
-        })
-      ]);
+    knex.schema.alterTable('tutorials', t => {
+      t.foreign('user_id')
+        .references('id')
+        .inTable('users');
+      t.foreign('instructor_id')
+        .references('id')
+        .inTable('instructors');
+    }),
+    knex.schema.alterTable('comments', t => {
+      t.foreign('user_id')
+        .references('id')
+        .inTable('users');
+      t.foreign('tutorial_id')
+        .references('id')
+        .inTable('tutorials');
+    }),
+    knex.schema.alterTable('tutorial_votes', t => {
+      t.foreign('user_id')
+        .references('id')
+        .inTable('users');
+      t.foreign('tutorial_id')
+        .references('id')
+        .inTable('tutorials');
+    }),
+    knex.schema.alterTable('comment_votes', t => {
+      t.foreign('user_id')
+        .references('id')
+        .inTable('users');
+      t.foreign('comment_id')
+        .references('id')
+        .inTable('comments');
+    }),
+    knex.schema.alterTable('favorites', t => {
+      t.foreign('user_id')
+        .references('id')
+        .inTable('users');
+      t.foreign('tutorial_id')
+        .references('id')
+        .inTable('tutorials');
+    })
+  ]);
 
-      console.log('foreign keys added');
-      resolve();
-    } catch (error) {
-      reject(error);
-    };
+  console.log('foreign keys added');
+};
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-        knex.schema.table('tutorials', t => {
-          t.dropForeign('user_id');
-          t.dropForeign('instructor_id');
-        }),
-        knex.schema.table('comments', t => {
-          t.dropForeign('user_id');
-          t.dropForeign('tutorial_id');
-        }),
-        knex.schema.table('tutorial_votes', t => {
-          t.dropForeign('user_id');
-          t.dropForeign('tutorial_id');
-        }),
-        knex.schema.table('comment_votes', t => {
-          t.dropForeign('user_id');
-          t.dropForeign('comment_id');
-        }),
-        knex.schema.table('favorites', t => {
-          t.dropForeign('user_id');
-          t.dropForeign('tutorial_id');
-        })
-      ]);
+    knex.schema.table('tutorials', t => {
+      t.dropForeign('user_id');
+      t.dropForeign('instructor_id');
+    }),
+    knex.schema.table('comments', t => {
+      t.dropForeign('user_id');
+      t.dropForeign('tutorial_id');
+    }),
+    knex.schema.table('tutorial_votes', t => {
+      t.dropForeign('user_id');
+      t.dropForeign('tutorial_id');
+    }),
+    knex.schema.table('comment_votes', t => {
+      t.dropForeign('user_id');
+      t.dropForeign('comment_id');
+    }),
+    knex.schema.table('favorites', t => {
+      t.dropForeign('user_id');
+      t.dropForeign('tutorial_id');
+    })
+  ]);
 
-      console.log('foreign keys dropped');
-      resolve();
-    } catch (error) {
-      reject(error);
-    };
+  console.log('foreign keys dropped');
+};
